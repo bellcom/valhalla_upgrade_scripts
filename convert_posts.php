cc1aeaec1d11dccc9408c4bb1f31a9ac93b37be1
Thomas Thune Hansen <tth@bellcom.dk>
2013-10-23 10:25:39 +0200

    updated convert posts script, create field instances for roles in field collection

--- convert_posts.php
+++ convert_posts.php
@@ -22,11 +22,35 @@ if(empty($roles)){
   $node->language = LANGUAGE_NONE;
   $node->title = 'VAF';
   node_save($node);
+
+  $roles = node_load_multiple(array(), array('type' => 'roles'));
+
+  foreach($roles as $role){
+    // Create the field base.
+    $field = array(
+    'field_name' => 'field_role_n'.$role->nid,
+    'type' => 'volunteers_roles_field',
+    );
+    field_create_field($field);
+    // Create the field instance on the bundle.
+    $instance = array(
+      'field_name' => 'field_role_n'.$role->nid,
+      'entity_type' => 'field_collection_item',
+      'label' => 'Role',
+      'bundle' => 'field_volunteers_pr_party_1',
+      // If you don't set the "required" property then the field wont be required by default.
+      'required' => TRUE,
+      'widget' => array(
+        'type' => 'custom_type',
+      ),
+    );
+  }
 }
 else {
-  echo "\nThere are some roles present in the installation, please make sure that there are (only) one role with the title VA and one with the title TI, otherwise stuff will most likely break\n\nPress [ENTER] to continue\n";
+  echo "\nThere are some roles present in the installation, check that there is correlation between role nodes and fieldcollection fields, otherwise stuff will break\n\nPress [ENTER] to continue\n";
   $line = fgets(STDIN);
 }
+
 $roles = node_load_multiple(array(), array('type' => 'roles'));
 
 foreach($roles as $role){
